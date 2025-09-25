fedab8cf2977a5758714609c6136320e1e88644b
Harsha <harshaginjaladec23@gmail.com>
2025-09-25 14:43:33 -0400

    Tested database and redis connection

--- ecommerce-api/app.py
+++ ecommerce-api/app.py
@@ -1,10 +1,24 @@
 from flask import Flask,request,url_for,render_template
+from utils import cache,database
+from sqlalchemy.exc import SQLAlchemyError
 
 
+app = Flask(__name__)
 
-
-core = Flask(__name__)
+@app.route('/health')
+def check_health():
+    try:
+        with database.engine.connect() as connection:
+            ans = "connection successful"
+    except SQLAlchemyError as e:
+        ans =f"connection unsuccessful {e} "
+    if cache.test_connection():
+        redis_answer = "success"
+    else:
+        redis_answer = "Unsuccessful"
+    
+    return f"database connection {ans} and redis connection {redis_answer}"
 
 
 if __name__ == "__main__":
-    core.run(debug=True)
+    app.run(host='0.0.0.0',port=5000,debug=True)
--- ecommerce-api/utils/database.py
+++ ecommerce-api/utils/database.py
@@ -1,32 +1,17 @@
 import os
-import psycopg2
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker,declarative_base
+from sqlalchemy.orm import sessionmaker, declarative_base
 
-# DATABSE DETAILS
+load_dotenv()
 
-DB_USER = os.getenv('DB_USER')
-DB_PASSWORD = os.getenv('DB_PASSWORD')
-DB_HOST = os.getenv('DB_HOST')
-DB_PORT = os.getenv('DB_PORT')
-DB_NAME = os.getenv('DB_NAME')
+DATABASE_URL = os.getenv('DATABASE_URL')
+print(f"DEBUG: DATABASE_URL = {DATABASE_URL}")
 
-## DATABASE URL
-
-DATABASE_URL = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+if DATABASE_URL is None:
+    raise ValueError("DATABASE_URL environment variable is not set!")
 
 engine = create_engine(DATABASE_URL)
 Base = declarative_base()
 
-
-try:
-    conn = psycopg2.connect(host = DB_HOST,database = DB_NAME
-                            ,user=DB_USER,password = DB_PASSWORD
-                            ,port = DB_PORT)
-    print("Successfully connected to PostgreSQL!")
-    conn.close()
-except psycopg2.Error as e:
-    print(f"Error connecting to PostgreSQL: {e}")
-
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, 
-  bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)

69d5c917b1b875e729eed62083d4d67d10fbe69f
Harsha <harshaginjaladec23@gmail.com>
2025-09-25 11:26:54 -0400

    docker file created

--- ecommerce-api/utils/cache.py
+++ ecommerce-api/utils/cache.py
@@ -1,7 +1,18 @@
 import redis
+import json
+import os
+from typing import Optional, Dict, Any
 
-redis_cli = redis.Redis(host='redis',port=6379,decode_responses=True)
+redis_client = redis.Redis(
+    host=os.getenv('REDIS_HOST', 'redis'),
+    port=int(os.getenv('REDIS_PORT', 6379)),
+    db=0,
+    decode_responses=True
+)
 
-connection = redis_cli.ping()
+def test_connection():
+    try:
+        return redis_client.ping()
+    except redis.ConnectionError:
+        return False
 
-print(connection)

dcf41efb982d36bcddef1a065c5b708291a0fd96
Harsha <harshaginjaladec23@gmail.com>
2025-09-29 12:16:23 -0400

    Implemented database models

--- ecommerce-api/models/product.py
+++ ecommerce-api/models/product.py
@@ -1,6 +1,6 @@
 from utils.database import Base
 from sqlalchemy.dialects.postgresql import UUID
-from sqlalchemy import Column,DATETIME,Integer
+from sqlalchemy import Column,DateTime,Integer
 from datetime import datetime
 import uuid
 
@@ -8,7 +8,7 @@ class Products(Base):
     __tablename__ = 'products'
     product_id = Column(UUID(as_uuid=True),primary_key=True,default=uuid.uuid4,index=True)
     stock_count = Column(Integer,nullable=False,default=0)
-    last_updated = Column(DATETIME,onupdate=datetime.utcnow)
+    last_updated = Column(DateTime,onupdate=datetime.utcnow)
 
     def __repr__(self):
         return f"<product {self.product_id} count: {self.stock_count}>"

11f5767b08320510136ddfd8c587381c64681175
Harsha <harshaginjaladec23@gmail.com>
2025-10-05 12:37:18 -0400

    added GraphQL schema for products

--- ecommerce-api/app.py
+++ ecommerce-api/app.py
@@ -4,11 +4,16 @@ from sqlalchemy.exc import SQLAlchemyError
 from routes.products import stocks
 from routes.orders import order_bp
 from routes.payments import payment_bp
+from graphql_api.schema import schema
+from strawberry.flask.views import GraphQLView
 
 app = Flask(__name__)
 app.register_blueprint(stocks)
 app.register_blueprint(order_bp)
 app.register_blueprint(payment_bp)
+
+app.add_url_rule('/graphql', view_func=GraphQLView.as_view("graphql_view",schema=schema))
+
 
 @app.route('/health')
 def check_health():
@@ -23,6 +28,10 @@ def check_health():
         redis_answer = "Unsuccessful"
     
     return f"database connection {ans} and redis connection {redis_answer}"
+
+@app.route("/")
+def home():
+    return "GraphQL API is running! Visit /graphql to test it."
 
 
 if __name__ == "__main__":

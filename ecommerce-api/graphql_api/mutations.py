0ba26425ef4170bbc066452a02db69095330f481
Harsha <harshaginjaladec23@gmail.com>
2025-10-06 19:27:56 -0400

    added GraphQL mutations for products

--- ecommerce-api/graphql_api/mutations.py
+++ ecommerce-api/graphql_api/mutations.py
@@ -1,7 +1,7 @@
 import strawberry
 from .types import Product,ProductInput,ProductUpdateInput,product_to_graphql
 from utils.database import SessionLocal,Base
-import uuid
+from uuid import UUID,uuid4
 from sqlalchemy.exc import DatabaseError
 from flask import jsonify
 from models.product import Products
@@ -13,7 +13,7 @@ class Mutation:
     @strawberry.mutation
     def create_product(self,input: ProductInput) ->Product :
         new_product = Products(
-            product_id=uuid.uuid4(),
+            product_id=uuid4(),
             price=input.price,
             stock_count=input.stock_count
         )
@@ -26,7 +26,7 @@ class Mutation:
     @strawberry.mutation
     def update_product(self,input: ProductUpdateInput) -> Optional[Product]:
         with SessionLocal() as db:
-         new_update = db.query(Products).filter(Products.product_id == input.product_id).first()
+         new_update = db.query(Products).filter(Products.product_id == UUID(input.product_id)).first()
          if not new_update:
             return None
          if input.price is not None:
@@ -38,7 +38,17 @@ class Mutation:
         db.refresh(new_update)
         return product_to_graphql(new_update)
 
-    
+    @strawberry.mutation
+    def delete_product(self,product_id : str) -> bool:
+        with SessionLocal() as db:
+            new_product = db.query(Products).filter(Products.product_id == UUID(product_id)).first()
+
+            if not new_product:
+                return False
+            db.delete(new_product)
+            db.commit()
+
+            return True
              
              
 

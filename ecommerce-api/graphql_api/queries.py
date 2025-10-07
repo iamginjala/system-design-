a9802db51db0f69efeabfd0d42a9be0edf3de17f
Harsha <harshaginjaladec23@gmail.com>
2025-10-07 12:07:00 -0400

    added cache to Graphql queries

--- ecommerce-api/graphql_api/queries.py
+++ ecommerce-api/graphql_api/queries.py
@@ -4,7 +4,7 @@ from utils.database import SessionLocal
 import strawberry
 from typing import Optional
 from uuid import UUID
-
+from utils.cache import get_data,set_data
 @strawberry.type 
 class Query:
     @strawberry.field
@@ -18,14 +18,27 @@ class Query:
             ]
     @strawberry.field
     def get_products_by_id(self,id: str) -> Optional[Product]:
-        with SessionLocal() as db:
-            try:
-                prod_by_id = db.query(Products).filter(Products.product_id == UUID(id)).first()
-                if prod_by_id:
-                    return  product_to_graphql(prod_by_id) 
-                return None
-            except ValueError as e:
-                return None
+        cached = get_data(id)
+        if isinstance(cached,dict):
+            print("cache hit - Retrieved from redis")
+            return product_to_graphql(cached) #type: ignore
+        else:
+            print("Cache miss- Querying from database")
+            with SessionLocal() as db:
+                try:
+                    prod_by_id = db.query(Products).filter(Products.product_id == UUID(id)).first()
+                    if prod_by_id:
+                        prod_data = {
+                            "product_id": id,
+                            "stock_count": prod_by_id.stock_count,
+                            "price": prod_by_id.price,
+                            "last_updated": prod_by_id.last_updated.isoformat() if prod_by_id.last_updated else None  # type: ignore
+                        }
+                        set_data(id,prod_data)
+                        return  product_to_graphql(prod_by_id) 
+                    return None
+                except ValueError as e:
+                    return None
 
 
 

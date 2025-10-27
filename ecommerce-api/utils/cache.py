61dc9172fd150b3661633c66d858a4b003b8de17
Harsha <harshaginjaladec23@gmail.com>
2025-10-27 14:54:56 -0400

    feat: add structured logging system

--- ecommerce-api/utils/cache.py
+++ ecommerce-api/utils/cache.py
@@ -2,11 +2,10 @@ import redis
 import json
 import os
 from typing import Optional, Dict, Any
-from .logger import get_app_logger,get_request_logger,get_error_logger
+from .logger import get_app_logger
 
-app_logger = get_app_logger()
-request_logger = get_request_logger()
-error_logger = get_error_logger()
+logger = get_app_logger()
+
 
 
 
@@ -35,10 +34,10 @@ def test_connection():
 def set_data(product_id,my_dict):
     try:
         redis_client.setex(f"product:{product_id}",60,json.dumps(my_dict))
-        app_logger.debug(f"data stored successfully under {product_id}")
+        logger.debug(f"data stored successfully under {product_id}")
         return True
     except Exception as e:
-        error_logger.error(f"Error message: {e}",exc_info=True)
+        logger.error(f"Error message: {e}",exc_info=True)
         return False
 
 def get_data(product_id):
@@ -46,18 +45,18 @@ def get_data(product_id):
         raw_data = redis_client.get(f"product:{product_id}")
 
         if raw_data is None or raw_data == "":
-            request_logger.info(f"no data found for key {product_id}")
+            logger.info(f"no data found for key {product_id}")
             return f"no data found for key {product_id}"
         if not isinstance(raw_data, (str, bytes, bytearray)):
-            error_logger.error(f"invalid data type for key {product_id}")
+            logger.error(f"invalid data type for key {product_id}")
             return f"invalid data type for key {product_id}"
         data = json.loads(raw_data)
         return data
     except json.JSONDecodeError:
-        error_logger.error(f"invalid json data for key {product_id}")
+        logger.error(f"invalid json data for key {product_id}")
         return f"invalid json data for key {product_id}"
     except Exception as e:
-        error_logger.error(f"Error message: {e}",exc_info=True)
+        logger.error(f"Error message: {e}",exc_info=True)
         return f"error retrieving dictionary {e}"
     
 
@@ -65,14 +64,14 @@ def delete_data(product_id):
     try:
         deleted = redis_client.delete(f"product:{product_id}")
         if deleted:
-            request_logger.info("cache deleted successfully")
+            logger.info("cache deleted successfully")
             return 'deleted successfully'
         else:
-            request_logger.info(f"no data found for {product_id}")
+            logger.info(f"no data found for {product_id}")
             return f"no data found for {product_id}"
 
     except Exception as e:
-        error_logger.error(f"No data found for this key {product_id}",exc_info=True)
+        logger.error(f"No data found for this key {product_id}",exc_info=True)
         return f"No data found for this key {product_id}"
 
 

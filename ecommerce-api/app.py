564a88b5c68f4196f68184d292adc5203f066fe9
Harsha <harshaginjaladec23@gmail.com>
2025-10-28 17:03:11 -0400

    feat: add structured logging system

--- ecommerce-api/app.py
+++ ecommerce-api/app.py
@@ -1,4 +1,4 @@
-from flask import Flask,request,render_template
+from flask import Flask,request,render_template,jsonify,g
 from utils import cache,database
 from sqlalchemy.exc import SQLAlchemyError
 from routes import stocks,order_bp,payment_bp,auth_bp
@@ -6,6 +6,8 @@ from graphql_api.schema import schema
 from strawberry.flask.views import GraphQLView
 import os 
 from dotenv import load_dotenv
+import time
+from utils.logger import get_request_logger,get_error_logger,get_app_logger
 
 
 load_dotenv()
@@ -17,6 +19,8 @@ def create_app():
     app.register_blueprint(order_bp)
     app.register_blueprint(payment_bp)
     app.register_blueprint(auth_bp)
+    logger = get_request_logger()
+    
 
     class CustomGraphQLView(GraphQLView):
         def get_context(self, request, response):
@@ -37,6 +41,29 @@ def create_app():
     DATABASE_URL = os.getenv('DATABASE_URL')
     if DATABASE_URL and DATABASE_URL.startswith('postgres://'):
         os.environ['DATABASE_URL'] = DATABASE_URL.replace('postgres://', 'postgresql://', 1)
+    
+    @app.before_request
+    def log_request():
+        
+        logger.info(f"incoming request : {request.method} path: {request.path} IP: {request.remote_addr}")
+        g.start_time = time.time()
+    
+    @app.after_request
+    def log_response(response):
+        # Use flask.g to store request-specific data (avoids accessing private Request attributes)
+        response_time = time.time() - getattr(g, "start_time", time.time())
+        response_time_ms = response_time * 1000
+        logger.info(f"response for {request.method} {request.path} completed in {response_time_ms:.2f}ms with status {response.status}")
+        # logger.info(f"response for {request.method} {request.path} completed in {response_time:.4f}s with status {response.statu0s}")
+        return response
+
+    @app.errorhandler(Exception)
+    def handle_exception(e):
+        err_logger = get_error_logger()
+        err_logger.error(f"exception {e} for {request.method} and path {request.path}",exc_info=True)
+
+        return jsonify({'error': 'Internal server error'}), 500
+
 
     @app.route('/health')
     def check_health():
